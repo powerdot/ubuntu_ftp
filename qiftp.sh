b32b516b2ac58ef8be14155701326e1d80cb9ba3
powerdot <donpsix@gmail.com>
2019-07-02 12:04:38 +0300

    Update qiftp.sh

--- qiftp.sh
+++ qiftp.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+
+# tw @power_dot 
+# Ilya R.
+
+# Check if user is root
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, use sudo sh $0"
+    exit 1
+fi
+
+
+
+# Script to add a user to Linux system
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -s -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p $pass $username
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system"
+	exit 2
+fi
+
+
+# Installing FTP
 echo "System update"
 apt-get update -y
 apt-get upgrade –y
